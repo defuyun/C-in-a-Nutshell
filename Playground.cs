e24c47a94133c1fe7caa77309443b88ec403ccd6
defuyun <carlliu2012@gmail.com>
2019-03-26 15:01:11 -0700

    add chapter 1 notes

---
+++ Playground.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace C__7._0_in_a_Nutshell
+{
+    class Program
+    {
+        static void Foo(ref StringBuilder fooSB) {
+            fooSB.Append("texture");
+            fooSB = null;
+        }
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            Foo(ref sb);
+            System.Console.WriteLine(sb);
+        }
+    }
+}
