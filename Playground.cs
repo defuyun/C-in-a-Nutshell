a8555b5a723acfdbb211fa3527ab7675ce5f900a
defuyun <carlliu2012@gmail.com>
2019-03-27 14:16:07 -0700

    complete chapter 3

--- Playground.cs
+++ Playground.cs
@@ -3,29 +3,12 @@ using System.Text;
 
 namespace C__7._0_in_a_Nutshell
 {
-
-    class Rectangle {
-        int number;
-
-        public int Number {
-            get {
-                System.Console.WriteLine("Enter getter");
-                return number;
-            }
-
-            set {
-                System.Console.WriteLine("Enter setter");
-                number = value;
-            }
-        }
-    }
     class Program
     {
+        static int Foo<T> (T x) => (int) (object) x;
         static void Main(string[] args)
         {
-            Rectangle rect = new Rectangle();
-            rect.Number = 10;
-            rect.Number -= 3;
+            System.Console.WriteLine(Foo(10));
         }
     }
 }
