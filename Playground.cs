4f7811b82a3e3afa30016b0ba0aa0be53e650019
defuyun <carlliu2012@gmail.com>
2019-03-29 12:45:02 -0700

    add chapter 4

--- Playground.cs
+++ Playground.cs
@@ -5,51 +5,19 @@ namespace C__7._0_in_a_Nutshell
 {
     class Program
     {
-        public class PriceChangedEventArgs : System.EventArgs {
-            public readonly float oldPrice;
-            public readonly float newPrice;
-
-            public PriceChangedEventArgs (float oldPrice, float newPrice) {
-                this.oldPrice = oldPrice;
-                this.newPrice = newPrice;
-            }
-        }
-
-        public class Stock {
-            float price;
-
-            public event EventHandler<PriceChangedEventArgs> PriceChanged;
-            // this is defined as delegate void EventHandler<Object, TEvent> (object source, TEvent evt);
-
-            protected virtual void OnPriceChanged(PriceChangedEventArgs e) {
-                PriceChanged?.Invoke(this, e);    
-            }
-
-            public float Price {
-                set {
-                    if (price == value) return;
-                    PriceChangedEventArgs args = new PriceChangedEventArgs(price, value);
-                    price = value;
-                    this.OnPriceChanged(args);
-                }
-                get {
-                    return price;
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            Stock stock = new Stock();
+            (string,int) bob = ("Bob", 23);
 
-            EventHandler<PriceChangedEventArgs> cb = (Object o, PriceChangedEventArgs e) => {
-                Stock t = o as Stock ?? new Stock();
-                System.Console.WriteLine(t.Price);  
-                System.Console.WriteLine(e.oldPrice);  
-            };
+            var joe = bob;
 
-            stock.PriceChanged += cb;
-            stock.Price = 12;
+            string b = bob.Item1;
+
+            b[1] = 'i';
+            joe.Item1[1] = 'i';
+
+            Console.WriteLine(bob.Item1);
+            Console.WriteLine(bob.Item2);
         }
     }
 }
