e9ae2784e78a2e15c34cbb86e16577c2587f50b3
defuyun <carlliu2012@gmail.com>
2019-03-28 17:51:02 -0700

    chapter 4

--- Playground.cs
+++ Playground.cs
@@ -3,15 +3,53 @@ using System.Text;
 
 namespace C__7._0_in_a_Nutshell
 {
-    public delegate string Reader();
-
-    public delegate void Writer(object s);
-
     class Program
     {
+        public class PriceChangedEventArgs : System.EventArgs {
+            public readonly float oldPrice;
+            public readonly float newPrice;
+
+            public PriceChangedEventArgs (float oldPrice, float newPrice) {
+                this.oldPrice = oldPrice;
+                this.newPrice = newPrice;
+            }
+        }
+
+        public class Stock {
+            float price;
+
+            public event EventHandler<PriceChangedEventArgs> PriceChanged;
+            // this is defined as delegate void EventHandler<Object, TEvent> (object source, TEvent evt);
+
+            protected virtual void OnPriceChanged(PriceChangedEventArgs e) {
+                PriceChanged?.Invoke(this, e);    
+            }
+
+            public float Price {
+                set {
+                    if (price == value) return;
+                    PriceChangedEventArgs args = new PriceChangedEventArgs(price, value);
+                    price = value;
+                    this.OnPriceChanged(args);
+                }
+                get {
+                    return price;
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
+            Stock stock = new Stock();
+
+            EventHandler<PriceChangedEventArgs> cb = (Object o, PriceChangedEventArgs e) => {
+                Stock t = o as Stock ?? new Stock();
+                System.Console.WriteLine(t.Price);  
+                System.Console.WriteLine(e.oldPrice);  
+            };
+
+            stock.PriceChanged += cb;
+            stock.Price = 12;
         }
     }
 }
