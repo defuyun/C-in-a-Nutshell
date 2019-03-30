58bf73b69c063f0f852067105844499d41a1d2d2
defuyun <carlliu2012@gmail.com>
2019-03-29 17:29:45 -0700

    add chapter 4

--- Playground.cs
+++ Playground.cs
@@ -1,23 +1,20 @@
 ﻿using System;
 using System.Text;
+using System.Runtime.CompilerServices;
+using System.Dynamic;
 
 namespace C__7._0_in_a_Nutshell
 {
     class Program
     {
+        static dynamic Mean(dynamic x, dynamic y) => (x + y)/2;
+
+        static void Foo([CallerLineNumber] int num = 0) => Console.WriteLine(num);
+
         static void Main(string[] args)
         {
-            (string,int) bob = ("Bob", 23);
-
-            var joe = bob;
-
-            string b = bob.Item1;
-
-            b[1] = 'i';
-            joe.Item1[1] = 'i';
-
-            Console.WriteLine(bob.Item1);
-            Console.WriteLine(bob.Item2);
+            Foo();
+            Foo();
         }
     }
 }
